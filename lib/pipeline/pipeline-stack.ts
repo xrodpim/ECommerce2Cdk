e2005198dd4cb4ce5354c1a134327a141de32d67
Rodrigo Pimenta <pimenta@inatel.br>
2021-07-28 09:25:29 -0300

    Primeiro commit.

---
+++ lib/pipeline/pipeline-stack.ts
@@ -0,0 +1,44 @@
+import * as codepipeline from "@aws-cdk/aws-codepipeline";
+import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions";
+import * as cdk from "@aws-cdk/core";
+import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
+
+export class PipelineStack extends cdk.Stack {
+
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+
+    super(scope, id, props);
+    const sourceArtifact = new codepipeline.Artifact();
+    const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+    const pipelines = new CdkPipeline(this, "Pipeline", {
+      pipelineName: "ECommercePipeline",
+      cloudAssemblyArtifact,
+
+      sourceAction: new codepipeline_actions.GitHubSourceAction({
+        actionName: "GitHub",
+        output: sourceArtifact,
+        oauthToken: cdk.SecretValue.secretsManager("github2-token"),
+        owner: "xrodpim",
+        repo: "ECommerce2Cdk",
+        branch: "master"
+      }),
+
+      synthAction: SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,
+        installCommand: "npm install -g aws-cdk",
+        buildCommand: "npm run build",
+        environment: {
+          privileged: true,
+        }
+
+      })
+
+    });
+
+    //TODO: estágio para fazer o deploy de fato.
+
+  }
+
+}
